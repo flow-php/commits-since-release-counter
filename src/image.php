ef9c81c43842763e610ebd050f40389d5c143298
Nyholm <tobias.nyholm@gmail.com>
2020-08-21 10:34:26 +0200

    Add CS rules

--- src/image.php
+++ src/image.php
@@ -2,15 +2,15 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-use SVG\SVG;
 use SVG\Nodes\Texts\SVGText;
+use SVG\SVG;
 
 $filename = $argv[1];
 $count = (int) $argv[2];
 
 $image = new SVG(90, 20);
-$suffix = $count === 1 ? 'commit' : 'commits';
-$svgText = new SVGText($count. ' '.$suffix, 4, 15);
+$suffix = 1 === $count ? 'commit' : 'commits';
+$svgText = new SVGText($count.' '.$suffix, 4, 15);
 
 $image->getDocument()->addChild($svgText);
 file_put_contents(dirname(__DIR__).'/docs/'.$filename.'.svg', $image->toXMLString());
