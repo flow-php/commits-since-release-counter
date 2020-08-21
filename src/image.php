79cbc324708ba1dcdf64a553d0b90081cccd94d1
Nyholm <tobias.nyholm@gmail.com>
2020-08-21 10:17:33 +0200

    Generaste images

--- src/image.php
+++ src/image.php
@@ -1,8 +1,15 @@
 <?php
 
+require_once __DIR__.'/../vendor/autoload.php';
+
+use SVG\SVG;
+use SVG\Nodes\Texts\SVGText;
+
 $filename = $argv[1];
 $count = $argv[2];
 
-// TODO create a proper SVG
+$image = new SVG(90, 20);
+$svgText = new SVGText($count. ' commits', 4, 15);
 
-file_put_contents(dirname(__DIR__).'/docs/'.$filename.'.svg', $count);
+$image->getDocument()->addChild($svgText);
+file_put_contents(dirname(__DIR__).'/docs/'.$filename.'.svg', $image->toXMLString());
